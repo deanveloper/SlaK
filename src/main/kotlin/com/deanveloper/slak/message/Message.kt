51bed29f8fa198d96aa0b9ceb62ac40e5c3f6386
Dean B <deanvelopermn@gmail.com>
2016-05-09 11:12:02 -0500

    Further improved message API

--- src/main/kotlin/com/deanveloper/slak/message/Message.kt
+++ src/main/kotlin/com/deanveloper/slak/message/Message.kt
@@ -11,36 +11,39 @@ import java.time.LocalDateTime
  * @author Dean B
  */
 class Message {
+    val type: String
     val owner: User
-    var message: String
+    var text: String
     val ts: LocalDateTime
     var starred: Boolean
     var reactions: List<Reaction>
 
-    constructor(owner: User,
+    constructor(type: String,
+                owner: User,
                 message: String,
                 ts: LocalDateTime,
                 starred: Boolean = false,
                 reactions: List<Reaction> = emptyList()) {
+        this.type = type
         this.owner = owner
-        this.message = message
+        this.text = message
         this.ts = ts
         this.starred = starred
         this.reactions = reactions
     }
 
     constructor(json: JsonObject) {
+        this.type = (json["subtype"] ?: json["type"]).asString
+        this.owner = User[json["user"].asString]
+        this.text = json["text"].asString
+        this.ts = json["ts"].asTimestamp
         this.starred = json["is_starred"]?.asBoolean ?: false
+
         this.reactions = json["reactions"]?.asJsonArray?.map { it.asJsonObject }
             ?.map {
-                Reaction(it["name"].asString,
-                    it["users"].asJsonArray.map { User[it.asString] }.toTypedArray()
-                )
+                Reaction(it["name"].asString, it["users"].asJsonArray.map { User[it.asString] }.toTypedArray())
             } ?: emptyList()
 
-        this.owner = User[json["user"].asString]
-        this.message = json["text"].asString
-        this.ts = json["ts"].asTimestamp
     }
 
     data class Reaction(val name: String, var users: Array<User>)
